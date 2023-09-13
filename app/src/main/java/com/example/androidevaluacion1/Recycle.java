ffcede7b69acf21e28bd02d3860ade1d66b6c792
bamc0 <bmunozcerezo@gmail.com>
2023-09-13 12:30:56 -0300

    Version 5

--- app/src/main/java/com/example/androidevaluacion1/Recycle.java
+++ app/src/main/java/com/example/androidevaluacion1/Recycle.java
@@ -1,14 +1,45 @@
 package com.example.androidevaluacion1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Recycle extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ItemAdapter itemAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recycle);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.rv);
+        itemAdapter = new ItemAdapter(this);
+
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        itemAdapter.setData(getData());
+        recyclerView.setAdapter(itemAdapter);
+        recyclerView.setLayoutManager(linearLayoutManager);
+    }
+
+    private List<Item> getData() {
+        List<Item> list = new ArrayList<>();
+        list.add(new Item(R.drawable.facebook, "Facebook"));
+        list.add(new Item(R.drawable.instagram, "Instagram"));
+        list.add(new Item(R.drawable.messenger, "Messenger"));
+        list.add(new Item(R.drawable.reddit, "Reddit"));
+        list.add(new Item(R.drawable.tumblr, "Tumblr"));
+        list.add(new Item(R.drawable.whatsapp, "WhatsApp"));
+        list.add(new Item(R.drawable.x, "X"));
+        list.add(new Item(R.drawable.youtube, "Youtube"));
+        return list;
+
     }
 }
