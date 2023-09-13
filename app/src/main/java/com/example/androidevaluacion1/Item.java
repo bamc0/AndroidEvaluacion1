a2c0284a1a2a11c6cf5e561d9ce00740670c5807
bamc0 <bmunozcerezo@gmail.com>
2023-09-13 12:23:18 -0300

    Version 4

---
+++ app/src/main/java/com/example/androidevaluacion1/Item.java
@@ -0,0 +1,28 @@
+package com.example.androidevaluacion1;
+
+public class Item {
+    private int image;
+    private String title;
+
+    public Item(int image, String title) {
+        this.image = image;
+        this.title = title;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
+}
